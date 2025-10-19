4f0045b976f63ed650e01570bc227409fe54bc1c
SprintingSnail <dashingcuddler@gmail.com>
2025-10-19 13:52:01 -0500

    im trying some js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('i dont know wtf js is')
+console.log('i dont know how to use js')
+//stole this from comet/vapor lol
+const gamblingNumbers = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+];
+//i think u give an html element an id right
+const button = document.getElementById('gamble-button');
+//empty slot for rolling #s?
+const slots = [
+    document.getElementById('slot1')
+    document.getElementById('slot2')
+    document.getElementById('slot3')
+]
+
+button.addEventListener('click') something idk
